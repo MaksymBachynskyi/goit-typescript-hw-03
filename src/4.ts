df9de2fa4ca763a0b8e575a24f19836d387476f1
MaksymBachynskyi <125116688+MaksymBachynskyi@users.noreply.github.com>
2023-11-26 18:10:31 +0200

    maked task 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key {
+	private signature: number;
+	constructor() {
+		this.signature = Math.random();
+	}
+	getSignature(this: Key): number {
+		return this.signature;
+	}
+}
+class Person {
+	private key: Key;
+	constructor(key: Key) {
+		this.key = key;
+	}
+	getKey(this: Person) {
+		return this.key;
+	}
+}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+abstract class House {
+	door: true | false;
+	key: Key;
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+	comeIn(person: Person) {
+		let tenants: Person[] = [];
+		if (this.door) {
+			tenants.push(person);
+		}
+		return tenants;
+	}
+	abstract openDoor(key: Key): boolean;
+}
+class MyHouse extends House {
+	constructor(key: Key) {
+		super();
+		this.key = key;
+	}
+	openDoor(key: Key): boolean {
+		if (key === this.key) {
+			return (this.door = true);
+		} else {
+			return (this.door = false);
+		}
+	}
+}
 
 export {};
